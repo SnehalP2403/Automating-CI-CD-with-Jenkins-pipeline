eff7bacf01069433c8557ad7bd0d642b4e5ca87c
Snehal Pandare <132730597+SnehalP2403@users.noreply.github.com>
2023-09-28 18:00:49 +0530

    Create security.tf
    Using resource

---
+++ security.tf
@@ -0,0 +1,17 @@
+resource "aws_security_group" "my_security_group" {
+  name_prefix = "my-sg-TF"
+  vpc_id      = module.vpc.vpc_id
+  
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
